663382ff9acb2f45a9babe5869f8ec32530f4145
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-01-16 13:18:44 +0600

    added height and wedth

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let a = +prompt("какова высота");
+let b = +prompt("какова ширина");
+
+let area  = a + b;
+console.log(area);
+
+// let c = +prompt("какой вес");
+// let d = +prompt("какой рост");
+// let tell = c + d;
+// console.log(tell);
+
